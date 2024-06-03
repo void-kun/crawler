56cdffa152dc473eb983116c5504f5b91d6434bd
void-kun <hoangzrik@gmail.com>
2024-06-03 08:22:29 +0700

    update

--- cosplaytele/src/download.py
+++ cosplaytele/src/download.py
@@ -2,14 +2,15 @@ import os
 import requests
 import shutil
 
+
 def download_file(url: str) -> str:
-    local_filename = url.split('/')[-1]
+    local_filename = url.split("/")[-1]
     print("local_filename: ", local_filename)
 
     with requests.get(url, stream=True) as r:
         r.raise_for_status()
 
-        with open(local_filename, 'wb') as f:
+        with open(local_filename, "wb") as f:
             shutil.copyfileobj(r.raw, f)
 
     return local_filename
